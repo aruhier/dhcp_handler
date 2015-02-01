5720117cbd4556721277e4b82adae0700725fea9
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-02-01 18:11:31 +0100

    Change the project structure
    Add config.py.default: copy it as config.py and change the configuration
                           in it.
    Move everything in the src directory
    Rename script

---
+++ dhcp_handler.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python2
+# Author: Anthony Ruhier
+# Check if IP has changed for a specific interface, and starts associated
+# handlers
+
+import os
+import sys
+import logging
+
+
+def prepare_logger(logger):
+    """
+    Set logger to a default level and add a steam handler
+    """
+    steam_handler = logging.StreamHandler()
+    steam_handler.setLevel(logging.DEBUG)
+    logger.addHandler(steam_handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+logger = prepare_logger(logging.getLogger())
+try:
+    from config import IF, SAVED_IP_PATH
+except ImportError:
+    logger.critical(
+        "The configuration file config.py doesn't exist. Please copy the "
+        "config.py.default file as config.py and launch this script again."
+    )
+    sys.exit(3)
+from src.ip_checker import IP_Checker_Daemon
+
+
+def print_help():
+    print("Script to handle IP changes on different interfaces.\n")
+    print("python dhcp_handler.py [option]")
+    print("[option]: ")
+    print("\tstart: start in background")
+    print("\tstop: stop the daemon")
+    print("\trestart: restart the daemon")
+    print("\trun: run in foreground")
+    exit()
+
+
+def init_dir(path):
+    """
+    Create target dir, where will be saved the IP of each interface
+    """
+    logger = logging.getLogger()
+    if not os.path.isdir(SAVED_IP_PATH):
+        try:
+            os.makedirs(SAVED_IP_PATH)
+        except FileExistsError:
+            logger.error("Error creating " + SAVED_IP_PATH + ", file already "
+                    "exists. Please remove it.")
+            sys.exit(1)
+
+
+init_dir(SAVED_IP_PATH)
+checker_daemon = IP_Checker_Daemon("/run/ip_checker_daemon.pid")
+
+if len(sys.argv) != 2:
+    print_help()
+elif "start" == sys.argv[1]:
+    checker_daemon.start()
+elif "stop" == sys.argv[1]:
+    checker_daemon.stop()
+elif "restart" == sys.argv[1]:
+    checker_daemon.restart()
+elif "run" == sys.argv[1]:
+    checker_daemon.run()
+else:
+    print_help()
--- src/ip_checker.py
+++ src/ip_checker.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python2
-# Check if IP has changed for a specific interface, and starts associated
-# handlers
+#!/bin/python
+# Author: Anthony Ruhier
 
 import logging
 import netifaces
@@ -8,41 +7,14 @@ import os
 import subprocess
 import sys
 import time
-from daemon import Daemon
-
-IF = ("eth0",)
-OLD_IP_PATH = "/var/lib/ip_update_handler"
-
-
-def prepare_logger(logger):
-    """
-    Set logger to a default level and add a steam handler
-    """
-    steam_handler = logging.StreamHandler()
-    steam_handler.setLevel(logging.DEBUG)
-    logger.addHandler(steam_handler)
-    logger.setLevel(logging.DEBUG)
-    return logger
-
-
-def init_dir(path):
-    """
-    Create target dir, where will be saved the IP of each interface
-    """
-    logger = logging.getLogger()
-    if not os.path.isdir(OLD_IP_PATH):
-        try:
-            os.makedirs(OLD_IP_PATH)
-        except FileExistsError:
-            logger.error("Error creating " + OLD_IP_PATH + ", file already "
-                    "exists. Please remove it.")
-            sys.exit(1)
+from src.daemon import Daemon
+from config import IF, REFRESH_TIME, SAVED_IP_PATH
 
 
 class IP_Checker_Daemon(Daemon):
-    logger = prepare_logger(logging.getLogger())
+    logger = logging.getLogger()
 
-    def get_ip():
+    def get_ip(self):
         """
         Get the 1st IPv4 associated to each interface in IF
         """
@@ -76,13 +48,17 @@ class IP_Checker_Daemon(Daemon):
             subprocess.call(["handlers/" + interface + ".sh", ip])
         except Exception as e:
             self.logger.error("Error when launching the handler " +
-                         interface + ".sh")
+                interface + ".sh")
             self.logger.error(e)
 
     def check_ip_changes(self):
+        """
+        Get current interfaces ip and compares it with the old ones
+        """
         ip_dict = self.get_ip()
         for interf, ip in ip_dict.items():
-            with open(os.path.join(OLD_IP_PATH, interf + "_ip"), "r+") as f:
+            with open(os.path.join(SAVED_IP_PATH, interf + "_ip"), "a+") as f:
+                f.seek(0)
                 old_ip = f.readline().rstrip(os.linesep)
                 if old_ip == ip:
                     self.logger.debug(interf + " ip didn't change")
@@ -92,28 +68,9 @@ class IP_Checker_Daemon(Daemon):
                     f.seek(0)
                     f.truncate()
                     f.write(ip + os.linesep)
-                    launch_handler(interf, ip)
+                    self.launch_handler(interf, ip)
 
     def run(self):
         while True:
             self.check_ip_changes()
-            time.sleep(5)
-
-
-init_dir(OLD_IP_PATH)
-checker_daemon = IP_Checker_Daemon("/run/ip_checker_daemon.pid")
-if len(sys.argv) == 2:
-    if 'start' == sys.argv[1]:
-        checker_daemon.start()
-    elif 'stop' == sys.argv[1]:
-        checker_daemon.stop()
-    elif 'restart' == sys.argv[1]:
-        checker_daemon.restart()
-    else:
-        print "Unknown command"
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
-    sys.exit(0)
-else:
-    print "usage: %s start|stop|restart" % sys.argv[0]
-    sys.exit(2)
+            time.sleep(REFRESH_TIME)

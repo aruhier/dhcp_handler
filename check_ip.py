6b76307289cbede4f4b07e83f2572364ec923ab1
Anthony Ruhier <anthony.ruhier@gmail.com>
2015-02-01 17:39:22 +0100

    Daemonize the script

--- check_ip.py
+++ check_ip.py
@@ -7,6 +7,8 @@ import netifaces
 import os
 import subprocess
 import sys
+import time
+from daemon import Daemon
 
 IF = ("eth0",)
 OLD_IP_PATH = "/var/lib/ip_update_handler"
@@ -37,56 +39,81 @@ def init_dir(path):
             sys.exit(1)
 
 
-def get_ip():
-    """
-    Get the 1st IPv4 associated to each interface in IF
-    """
-    logger = logging.getLogger()
-    ip_assoc = dict()
-    for interf in IF:
+class IP_Checker_Daemon(Daemon):
+    logger = prepare_logger(logging.getLogger())
+
+    def get_ip():
+        """
+        Get the 1st IPv4 associated to each interface in IF
+        """
+        ip_assoc = dict()
+        for interf in IF:
+            try:
+                ip_assoc[interf] = (
+                    netifaces.ifaddresses(interf)[netifaces.AF_INET][0]["addr"]
+                )
+            except IndexError:
+                self.logger.error("No IPv4 set for " + interf)
+                continue
+            except ValueError:
+                self.logger.error(
+                    "Interface " + interf + " doesn't exist (yet)")
+                continue
+            except Exception as e:
+                self.logger.error(e)
+                continue
+        return ip_assoc
+
+    def launch_handler(self, interface, ip):
+        """
+        Launch associated handler
+        """
+        handler_path = os.path.join("handlers", interface + ".sh")
+        if not os.path.isfile(handler_path):
+            self.logger.info("No handler found for " + interface + ".")
+            return
         try:
-            ip_assoc[interf] = (
-                netifaces.ifaddresses(interf)[netifaces.AF_INET][0]["addr"]
-            )
-        except IndexError:
-            logger.error("No IPv4 set for " + interf)
-            continue
-        except ValueError:
-            logger.error("Interface " + interf + " doesn't exist (yet)")
-            continue
+            subprocess.call(["handlers/" + interface + ".sh", ip])
         except Exception as e:
-            logger.error(e)
-            continue
-    return ip_assoc
+            self.logger.error("Error when launching the handler " +
+                         interface + ".sh")
+            self.logger.error(e)
 
+    def check_ip_changes(self):
+        ip_dict = self.get_ip()
+        for interf, ip in ip_dict.items():
+            with open(os.path.join(OLD_IP_PATH, interf + "_ip"), "r+") as f:
+                old_ip = f.readline().rstrip(os.linesep)
+                if old_ip == ip:
+                    self.logger.debug(interf + " ip didn't change")
+                else:
+                    self.logger.debug(
+                        interf + " ip changed, launching handler")
+                    f.seek(0)
+                    f.truncate()
+                    f.write(ip + os.linesep)
+                    launch_handler(interf, ip)
 
-def launch_handler(interface, ip):
-    """
-    Launch associated handler
-    """
-    logger = logging.getLogger()
-    handler_path = os.path.join("handlers", interface + ".sh")
-    if not os.path.isfile(handler_path):
-        logger.info("No handler found for " + interface + ".")
-        return
-    try:
-        subprocess.call(["handlers/" + interface + ".sh", ip])
-    except Exception as e:
-        logger.error("Error when launching the handler " + interface + ".sh")
-        logger.error(e)
+    def run(self):
+        while True:
+            self.check_ip_changes()
+            time.sleep(5)
 
 
 init_dir(OLD_IP_PATH)
-logger = prepare_logger(logging.getLogger())
-ip_dict = get_ip()
-for interf, ip in ip_dict.items():
-    with open(os.path.join(OLD_IP_PATH, interf + "_ip"), "r+") as f:
-        old_ip = f.readline().rstrip(os.linesep)
-        if old_ip == ip:
-            logger.debug(interf + " ip didn't change")
-        else:
-            logger.debug(interf + " ip changed, launching handler")
-            f.seek(0)
-            f.truncate()
-            f.write(ip + os.linesep)
-            launch_handler(interf, ip)
+checker_daemon = IP_Checker_Daemon("/run/ip_checker_daemon.pid")
+if len(sys.argv) == 2:
+    if 'start' == sys.argv[1]:
+        checker_daemon.start()
+    elif 'stop' == sys.argv[1]:
+        checker_daemon.stop()
+    elif 'restart' == sys.argv[1]:
+        checker_daemon.restart()
+    else:
+        print "Unknown command"
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
+    sys.exit(0)
+else:
+    print "usage: %s start|stop|restart" % sys.argv[0]
+    sys.exit(2)
